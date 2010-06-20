0fc9c720e32aa807eb4c2bdb22a057ec1173a0b6
George Brocklehurst <george.brocklehurst@gmail.com>
2010-06-20 16:49:58 +0100

    Actual worky bits, based on Tom's original add on.
    http://github.com/cwninja/choosy-osx-plugin

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -10,6 +10,13 @@ var choosy = {
 
   onToolbarButtonCommand: function(event) {
     
+  },
+
+  promptAll: function(url) {
+    var ioservice = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
+    var uriToOpen = ioservice.newURI("x-choosy://prompt.all/" + escape(url), null, null);
+    var extps = Components.classes["@mozilla.org/uriloader/external-protocol-service;1"].getService(Components.interfaces.nsIExternalProtocolService);
+    extps.loadURI(uriToOpen, null);
   }
 };
 
