decccd51263df541aec8ab208354094725496258
George Brocklehurst <george.brocklehurst@gmail.com>
2010-06-20 16:48:38 +0100

    The extension generator leaves a lot of mess.

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,20 +1,15 @@
 var choosy = {
   onLoad: function() {
-    // initialization code
     this.initialized = true;
     this.strings = document.getElementById("choosy-strings");
   },
 
-  onMenuItemCommand: function(e) {
-    var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-                                  .getService(Components.interfaces.nsIPromptService);
-    promptService.alert(window, this.strings.getString("helloMessageTitle"),
-                                this.strings.getString("helloMessage"));
+  onMenuItemCommand: function(event) {
+    
   },
 
-  onToolbarButtonCommand: function(e) {
-    // just reuse the function above.  you can change this, obviously!
-    choosy.onMenuItemCommand(e);
+  onToolbarButtonCommand: function(event) {
+    
   }
 };
 
--- defaults/preferences/prefs.js
+++
@@ -1,6 +0,0 @@
-pref("extensions.choosy.boolpref", false);
-pref("extensions.choosy.intpref", 0);
-pref("extensions.choosy.stringpref", "A string");
-
-// https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.firefox.choosyosx.com.description", "chrome://choosy/locale/overlay.properties");
